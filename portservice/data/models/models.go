9da51938367565d51fb4bcaf9507c56b30c09f86
Fabiano Graças <fabiano.gracas@daimler.com>
2021-02-18 02:50:37 +0100

    remove id in order to mongoDb generate this id.

--- portservice/data/models/models.go
+++ portservice/data/models/models.go
@@ -1,13 +1,11 @@
 package models
 
 import (
-    "go.mongodb.org/mongo-driver/bson/primitive"
     "time"
 )
 
 //Issue - struct to map with mongodb documents
 type AirportDB struct {
-    ID          primitive.ObjectID `bson:"_id"`
     CreatedAt   time.Time          `bson:"created_at"`
     UpdatedAt   time.Time          `bson:"updated_at"`
     Name        string             `bson:"name"`
