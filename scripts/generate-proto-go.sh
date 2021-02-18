6e0588c80624c6a886f7b2fa2a211801fd483a45
Fabiano Graças <fabiano.gracas@daimler.com>
2021-02-18 02:53:35 +0100

    generate clients also to client api

--- scripts/generate-proto-go.sh
+++ scripts/generate-proto-go.sh
@@ -2,3 +2,6 @@
 
 protoc -I "${PWD}/../portservice" --go_out="${PWD}/../" "${PWD}/../portservice/internal/proto-files/domain/port.proto"
 protoc -I "${PWD}/../" --go_out=plugins=grpc:"${PWD}/../" "${PWD}/../portservice/internal/proto-files/service/port-service.proto"
+
+protoc -I "${PWD}/../portservice" --go_out="${PWD}/../clientapi/" "${PWD}/../portservice/internal/proto-files/domain/port.proto"
+protoc -I "${PWD}/../" --go_out=plugins=grpc:"${PWD}/../clientapi/" "${PWD}/../portservice/internal/proto-files/service/port-service.proto"
